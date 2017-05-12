a55adba2b8aaf91524e97bf2f1792379c5d2c290
Divij <divij.mehra.16@gmail.com>
2017-05-12 17:30:13 +1000

    added in api

--- controllers/song_controller.js
+++ controllers/song_controller.js
@@ -9,6 +9,14 @@ exports.getSong = (req, res) => {
     });
 };
 
+exports.getSongApi = (req, res) => {
+  Song.find()
+    .then((allsongs) => {
+      // index refers to the to the file name index in the views folder
+      res.json(allsongs)
+    });
+};
+
 exports.createSong = (req, res) => {
   console.log(req)
   const artist = req.body.artist;
@@ -26,12 +34,37 @@ exports.createSong = (req, res) => {
     })
 };
 
+exports.createSongApi = (req, res) => {
+  const artist = req.query.artist;
+  const song_name = req.query.song_name;
+  const year = req.query.year;
+
+  let song = new Song ();
+  song.artist = artist;
+  song.name = song_name;
+  song.year = year;
+
+  song.save()
+    .then(() => {
+      res.redirect('/api');
+    })
+};
+
 exports.getEdit = (req, res) => {
   // edit refers to the file name edit in the views folder
   // Make sure that you start the model/class with a capital letter mate!
   Song.findOne({_id: req.params.id})
     .then(song => {
         res.render('edit', {song: song})
+    })
+};
+
+exports.getSingleSongApi = (req, res) => {
+  // edit refers to the file name edit in the views folder
+  // Make sure that you start the model/class with a capital letter mate!
+  Song.findOne({_id: req.params.id})
+    .then(song => {
+        res.json(song)
     })
 };
 
@@ -46,9 +79,27 @@ exports.updateSong = (req, res) => {
     });
 };
 
+exports.updateSongApi = (req, res) => {
+  // edit refers to the file name edit in the views folder
+  // findOneAndUpdate needs three arguements to be passed through! Id, body and this new shit (READ ME - read the docs)
+  Song.findOneAndUpdate({ _id: req.params.id}, req.query, {
+    new: true
+  })
+    .then(() => {
+      res.redirect(`/api/${req.params.id}`);
+    });
+};
+
 exports.deleteSong = (req,res) => {
   Song.findOneAndRemove({ _id: req.params.id})
     .then(() => {
       res.redirect('/');
+    });
+};
+
+exports.deleteSongApi = (req,res) => {
+  Song.findOneAndRemove({ _id: req.params.id})
+    .then(() => {
+      res.redirect('/api');
     });
 };
--- routes/index.js
+++ routes/index.js
@@ -16,3 +16,19 @@ router.post('/:id/edit', SongController.updateSong);
 router.post('/:id/delete', SongController.deleteSong);
 
 module.exports = router;
+
+// APIs
+
+// GET API
+
+router.get('/api', SongController.getSongApi);
+router.post('/api', SongController.createSongApi);
+
+// EDIT API
+
+router.get('/api/:id', SongController.getSingleSongApi);
+router.post('/api/:id/edit', SongController.updateSongApi);
+
+// DELETE API
+
+router.delete('/api/:id/delete', SongController.deleteSongApi);
